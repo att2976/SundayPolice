0f5dc19a3a62f453fd100cdf9240e7f0ed4ad7cb
Austin Tyler <att2976@att.net>
2019-06-23 14:19:21 -0400

    Ben fixes my bot

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ client.once('ready', () => {
 	console.log('Ready!')
 })
 
-var today = new Date();
-  var day = today.getDay();
+
   var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-  console.log("Today is : " + daylist[day] + ".");
+  // console.log("Today is : " + daylist[day] + ".");
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-	day = today.getDay();
+	var today = new Date();
+  	var day = today.getDay();
 	console.log("Today is : " + daylist[day] + ".");
 	let newChannel = newMember.voiceChannelID;
 	if(newChannel == config.SunChan && daylist[day] != "Sunday"){
